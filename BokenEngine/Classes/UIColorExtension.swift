04460ae2de0c3e78576a6618b1d4360b4800179c
Fer Torres <fernando.torres@hyve.net>
2021-03-16 13:01:44 +0000

    Apply the Mozilla Public License
    Add the header from Exhibit A of the license to each source file in the project

--- BokenEngine/Classes/UIColorExtension.swift
+++ BokenEngine/Classes/UIColorExtension.swift
@@ -1,10 +1,8 @@
-//
-//  UIColorExtention.swift
-//  Boken Engine
-//
-//  Created by Yeray on 12/11/2020.
-//  Copyright © 2020 Hyve. All rights reserved.
-//
+/* Copyright 2021 The Boken Authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 import UIKit
 
--- BokenEngineTests/SceneFactoryTests.swift
+++ BokenEngineTests/SceneFactoryTests.swift
@@ -1,10 +1,8 @@
-//
-//  SceneFactoryTests.swift
-//  Scene Factory Tests
-//
-//  Created by Yeray on 13/11/2020.
-//  Copyright © 2020 Hyve. All rights reserved.
-//
+/* Copyright 2021 The Boken Authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 import XCTest
 @testable import BokenEngine
