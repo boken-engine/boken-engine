659e96aa1702b8450dbbcc792cd39540a81b4378
Yeray Rodriguez <yeray.rodriguez@edataconsulting.es>
2021-08-25 07:22:45 +0100

    Fix trailing commas on Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BokenEngine",
-            targets: ["BokenEngine"]),
+            targets: ["BokenEngine"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -28,6 +28,6 @@ let package = Package(
             name: "BokenEngineTests",
             dependencies: ["BokenEngine", "PathKit"],
             resources: [
-                .process("Resources")]),
+                .process("Resources")])
     ]
 )
